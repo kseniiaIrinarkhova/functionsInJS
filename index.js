e8c282f5c3e1b187de524d304f85813c3adf2b0f
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-01-19 18:21:02 -0800

    used map method

--- index.js
+++ index.js
@@ -88,9 +88,6 @@ printAllNumbetsBetween(n);
 let input = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" }, { id: "48", name: "Barry", occupation: "Runner", age: "25" }, { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" }, { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" }, { id: "7", name: "Bilbo", occupation: "None", age: "111" }];
 
 // Use callback functions alongside Array methods to accomplish the following:
-
-
-// Map the array to change the “occupation” key to “job” and increment every age by 1.
 // Use the reduce method to calculate the sum of the ages.
 // Then use the result to calculate the average age.
 
@@ -111,3 +108,15 @@ console.log(input);
 let filteredArray = input.filter((x) => x.age <= 50); 
 console.log("\n Filtered:")
 console.log(filteredArray);
+
+// Map the array to change the “occupation” key to “job” and increment every age by 1.
+let mappedArray = input.map((element) => {
+    return{
+        id : element.id,
+        name : element.name,
+        job : element.occupation,
+        age : ++element.age
+    }
+})
+console.log("\n Mapped:")
+console.log(mappedArray);
